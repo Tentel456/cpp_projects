d8a1f94b7bdff971029f85fbb78e431b0efa3a20
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-06 23:22:19 +0300

    creating menu

---
+++ bank/menu.cpp
@@ -0,0 +1,11 @@
+#include "menu.h"
+#include <iostream>
+
+void menu()
+{
+    std::cout << "Выберите нужную функцию:\n";
+    std::cout << "1. Пополнить баланс\n";
+    std::cout << "2. Вывести сумму\n";
+    std::cout << "3. Посмотреть баланс\n";
+    std::cout << "4. Выйти из приложения\n";
+}
