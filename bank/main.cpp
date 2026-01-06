35addfd2561a7126ecabae2a9fa87575b854f783
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-06 23:24:17 +0300

    Update main.cpp

--- bank/main.cpp
+++ bank/main.cpp
@@ -1,6 +1,7 @@
 #include "welcome.h"
 #include "deposit.h"
 #include "withdraw.h"
+#include "menu.h"
 #include <iostream>
 #include <cstdlib>
 
@@ -11,5 +12,30 @@ int main()
 
     bool active { true };
     welcome();
+
+    while ( active )
+    {
+
+        menu();
+
+        int choise {};
+        std::cin >> choise;
+
+        switch ( choise )
+        {
+        case 1:
+            balance = deposit(balance);
+            break;
+        
+        case 2:
+            balance = withdraw(balance);
+            break;
+        }
+    }
+
+
     return EXIT_SUCCESS;
 }
+
+
+
