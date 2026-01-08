7701dea9d6e8e6aad5570f5e4b18ae305021288e
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-08 19:11:50 +0300

    small input func update

--- crypto/input.cpp
+++ crypto/input.cpp
@@ -10,8 +10,8 @@ int input()
     {
         std::cout << "Предлагаем вложить все ваши средства в критповалюту Bitcoin, поскольку сумма небольшая\n";
         std::cout << "Вкладываем?\n";
-        std::cout << "1. Да";
-        std::cout << "2. Нет";
+        std::cout << "1. Да\n";
+        std::cout << "2. Нет\n";
         int choice {};
         std::cin >> choice;
 
@@ -22,15 +22,21 @@ int input()
             switch (choice)
             {
                 case 1:
-                std::cout << "Ваша сумма будет вложена в Bitcoin!";
+                std::cout << "Ваша сумма будет вложена в Bitcoin!\n";
                 break;
 
                 case 2:
-                std::cout << "Вы вышли из программы!";
+                std::cout << "Вы вышли из программы!\n";
                 active = false;
                 break;
             }
         }
+    }
+
+    else if ( sum > 2500 and sum <= 10000)
+    {
+        std::cout << "У вас уже неплохие накопления!\n";
+        std::cout << "Предлагаем такое распределение средств:\n";
     }
     return sum;
 }
