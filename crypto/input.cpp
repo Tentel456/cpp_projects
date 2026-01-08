7dc8c83d7405542a4d22187c3826ec7da2fc8c78
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-08 18:59:15 +0300

    minor input func update

--- crypto/input.cpp
+++ crypto/input.cpp
@@ -8,8 +8,29 @@ int input()
     std::cin >> sum;
     if ( sum < 2500 )
     {
-        std::cout << "Предлагаем вложить все ваши средства в критповалюту Bitcoin, поскольку сумма небольшая";
-        std::cout << "Вкладываем?";
+        std::cout << "Предлагаем вложить все ваши средства в критповалюту Bitcoin, поскольку сумма небольшая\n";
+        std::cout << "Вкладываем?\n";
+        std::cout << "1. Да";
+        std::cout << "2. Нет";
+        int choice {};
+        std::cin >> choice;
+
+        bool active { true };
+
+        while ( active )
+        {
+            switch (choice)
+            {
+                case 1:
+                std::cout << "Ваша сумма будет вложена в Bitcoin!";
+                break;
+
+                case 2:
+                std::cout << "Вы вышли из программы!";
+                active = false;
+                break;
+            }
+        }
     }
     return sum;
 }
