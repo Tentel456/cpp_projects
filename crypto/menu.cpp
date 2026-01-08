74b8d76ae534ad2bbfbe91251aee3bb71e6f3d86
Tentel456 <126483734+Tentel456@users.noreply.github.com>
2026-01-08 19:55:20 +0300

    menu func update

--- crypto/menu.cpp
+++ crypto/menu.cpp
@@ -1,7 +1,14 @@
 #include "menu.h"
+#include "symbol.h"
 #include <iostream>
 
 void menu()
 {
     std::cout << "Меню:\n";
+    int balance {1000}; //Пока заглушка
+    std::cout << "Ваш баланс:" << balance << symbol() << std::endl; //Добавить переменную баланс
+    std::cout << "Ваши криптоактивы:";
+    int bitcoin {};
+    int ethereum {};
+    int usdt {};
 }
